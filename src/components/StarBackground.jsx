7a7234ab89daf2e541b47e5e9123e904685a976c
Shivam Bhardwaj <bhardwajshivambsr2000@gmail.com>
2025-07-27 23:05:14 +0530

    Added Navbar and StarBackground components; updated Home page and styles

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+
+//StarBAckground must contain a id, size, x, y, opacity, animationDuration.
+
+const StarBackground = () => {
+    const [stars, setStars] = useState([]);
+    const [meteors, setMeteors] = useState([]);
+
+    useEffect(() => {
+        generateStars();
+        generateMeteors();
+
+        const handleResize = () => {
+            generateStars();
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            setStars([]);
+            setMeteors([]);
+        }
+
+    }, []);
+
+    const generateMeteors = () => {
+        const numberOfMeteors = 5;
+        const newMeteors = [];
+        
+        for(let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id: i,
+                size: Math.random() * 2 + 1, // Random size between 1 and 3
+                x: Math.random() * 100, // Random x position in percentage
+                y: Math.random() * 20, // Random y position in percentage
+                delay: Math.random() * 15, // Random delay for animation
+                opacity: Math.random() * 0.5 + 0.5, // Random opacity between 0.5 and 1
+                animationDuration: Math.random() * 3 + 3 // Random duration between 2 and 6 seconds
+            });
+        }
+        setMeteors(newMeteors);
+    }
+
+    const generateStars = () => {
+        const numberOfStars = Math.floor(window.innerWidth* window.innerHeight / 10000);
+        const newStars = [];
+        
+        for(let i = 0; i < numberOfStars; i++) {
+            newStars.push({
+                id: i,
+                size: Math.random() * 2 + 1, // Random size between 1 and 3
+                x: Math.random() * 100, // Random x position in percentage
+                y: Math.random() * 100, // Random y position in percentage
+                opacity: Math.random() * 0.5 + 0.5, // Random opacity between 0.5 and 1
+                animationDuration: Math.random() * 4 + 1 // Random duration between 2 and 6 seconds
+            });
+        }
+        setStars(newStars);
+    }
+
+  return (
+  <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+    {stars.map((star) => (
+        <div key={star.id} className='star animate-pulse-subtle' style={{
+               width: `${star.size}px`,
+                height: `${star.size}px`,
+                left: `${star.x}%`,
+                top: `${star.y}%`,
+                opacity: star.opacity,
+                animationDuration: `${star.animationDuration}s`  
+        }} />
+    ))}
+
+    {meteors.map((meteor) => (
+        <div key={meteor.id} className='meteor animate-meteor meteorFall' style={{
+               width: `${meteor.size*40}px`,
+                height: `${meteor.size*2}px`,
+                left: `${meteor.x}%`,
+                top: `${meteor.y}%`,
+                animationDelay: meteor.delay,
+                animationDuration: `${meteor.animationDuration}s`  
+        }} />
+    ))}
+  </div>
+  )
+}
+
+export default StarBackground
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Navbar from "../components/Navbar";
+import StarBackground from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
+
+
 
 export const Home = () => {
 
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
         {/* theme */ }
         <ThemeToggle />
+        <StarBackground />
+        <Navbar />
     </div>
 };
