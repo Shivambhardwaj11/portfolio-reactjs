894a88d16257e1d2a6ff7dc84ac5cbf5c5b0484a
Shivam Bhardwaj <bhardwajshivambsr2000@gmail.com>
2025-08-04 19:01:11 +0530

    Added the SKill Section to the portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { X } from 'lucide-react';
 const navItems = [
     {name: 'Home', href:"#hero"},
     {name: 'About', href:"#about"},
-    {name: 'Skills', href:"#skill"},
+    {name: 'Skills', href:"#skills"},
     {name: 'Projects', href:"#projects"},
     {name: 'Contact', href:"#contact"},
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutMe } from "../components/AboutMe";
 import { HeroSection } from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import { SkillSection } from "../components/SkillSection";
 import StarBackground from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -17,6 +18,7 @@ export const Home = () => {
         <main>
             <HeroSection />
             <AboutMe />
+            <SkillSection />
         </main>
     </div>
 };
